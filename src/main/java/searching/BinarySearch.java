0ac5c00b3fac56bedd26487fa3a89cfd658c5ee6
agatarauzer <agatarauzer@gmail.com>
2021-01-24 14:55:47 +0100

    small correction in binary search algorithm

--- src/main/java/searching/BinarySearch.java
+++ src/main/java/searching/BinarySearch.java
@@ -1,16 +1,12 @@
 package searching;
 
 public class BinarySearch {
-
     public static boolean searchNumber(int[] array, int searched) {
-
         int low = 0;
         int high = array.length - 1;
-
         while (low <= high) {
-            int mid = (low + high) / 2;
+            int mid = low + ((high - low)/2);
             int guess = array[mid];
-
             if (searched == guess) {
                 return true;
             }
